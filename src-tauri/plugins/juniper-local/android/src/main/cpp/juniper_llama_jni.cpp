bf3b25954a1583badcda21e72540b4eecdaa7ee9
Cinqic <meantanimalza@gmail.com>
2026-09-07 03:15:45 -0400

    Force CPU layers for Android inference

--- src-tauri/plugins/juniper-local/android/src/main/cpp/juniper_llama_jni.cpp
+++ src-tauri/plugins/juniper-local/android/src/main/cpp/juniper_llama_jni.cpp
@@ -88,6 +88,7 @@ struct Engine {
         if (!validate_gguf_metadata(path)) return 3;
 
         llama_model_params model_params = llama_model_default_params();
+        model_params.n_gpu_layers = 0;
         model = llama_model_load_from_file(path.c_str(), model_params);
         if (model == nullptr) return 9;
         if (!llama_model_has_decoder(model)) {
